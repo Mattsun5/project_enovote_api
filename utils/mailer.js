51da28c8fa1e929c710e26fbeb2302b72f085112
Matthew <sundaymatt703@gmail.com>
2025-12-18 22:28:36 +0100

    smtp for prod

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,25 +1,25 @@
 import nodemailer from "nodemailer";
 
 // DEVELOPMENT
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS, // Gmail App Password
-  },
-  tls: {
-    rejectUnauthorized: false, // 👈 FIX
-  },
-});
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS, // Gmail App Password
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 
 // PRODUCTION
-// export const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: Number(process.env.SMTP_PORT),
-//   secure: false,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS,
-//   },
-// });
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
