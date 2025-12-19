bc6ccfb9822013939d1a60629425bd6110a4ce2a
Matthew <sundaymatt703@gmail.com>
2025-12-19 09:53:25 +0100

    allow unauthorized tls

--- utils/mailer.js
+++ utils/mailer.js
@@ -22,4 +22,7 @@ export const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+      rejectUnauthorized: false
+   }
 });
